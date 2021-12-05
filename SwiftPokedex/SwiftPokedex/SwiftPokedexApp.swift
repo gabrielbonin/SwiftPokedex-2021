fc54086ab7e37b55b04369d9bbbd4caec4ad4f70
Gabriel Bonin <gabriel.bonin@mblabs.com.br>
2021-12-04 22:01:12 -0300

    Base Project

---
+++ SwiftPokedex/SwiftPokedex/SwiftPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPokedexApp.swift
+//  SwiftPokedex
+//
+//  Created by Gabriel MBLABS on 04/12/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
